a1ba0ece477b0a9848b9c610939ba3e30edd9635
deybi payco <deybi.payco@vallegrande.edu.pe>
2025-09-09 14:15:06 -0500

    creacion de cantacnos.html y productos.html

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template ('index.html')
 
+@app.route("/productos")
+def productos():
+    return render_template("productos.html")
+
+@app.route('/contact')
+def contactanos():
+    return render_template('contactanos.html')
 
 #ejecutar mi servidor
 if __name__ == '__main__':
